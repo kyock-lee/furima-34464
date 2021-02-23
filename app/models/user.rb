e50786ae27f9ae23bf57536b8bb818877e899c7a
kyock-lee <yakioni1031@gmailcom>
2021-02-23 11:14:48 +0900

    Userモデルのバリデーション(修正1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :last_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
-
-
-
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end

29ec60327fca03451008350e639c0016e4c6188d
kyock-lee <yakioni1031@gmailcom>
2021-02-24 20:15:15 +0900

    Itemモデルのバリデーション(修正1)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,12 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   has_one_attached :image
+  validates :cost, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than: 10000000}
 
   with_options presence: true do
     validates :image
     validates :item_name
     validates :description
-    validates :cost, format: { with: /\A[0-9]+\z/ }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 999999999}
-
     
     with_options numericality: { other_than: 1 } do
       validates :category_id

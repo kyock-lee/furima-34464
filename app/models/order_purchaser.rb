3d9a5120e66ba068e7877ba7326766dadf698de6
kyock-lee <yakioni1031@gmailcom>
2021-03-02 16:32:21 +0900

    DBに保存する処理を記述
    OrderPurchaserクラス

--- app/models/order_purchaser.rb
+++ app/models/order_purchaser.rb
@@ -3,14 +3,21 @@ class OrderPurchaser
   attr_accessor :post_code, :shipping_area_id, :shipping_city, :shipping_adress, :building_name, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :shipping_area_id
     validates :shipping_city
     validates :shipping_adress
     validates :phone_number, format: {with: /\A[0-9]{11}+\z/}
-    
+
     validates :order_id
     validates :user_id
     validates :item_id
   end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    #購入者情報)を保存する
+    Purchaser.create(post_code: post_code, shipping_area_id: shipping_area_id, shipping_city: shipping_city, shipping_adress: shipping_adress, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+
 end

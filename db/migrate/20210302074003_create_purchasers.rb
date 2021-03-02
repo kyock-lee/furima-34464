7453fa53d5b88bbb5ebab1268c177c22bf5a7f70
kyock-lee <yakioni1031@gmailcom>
2021-03-02 18:21:37 +0900

    マイグレーションファイル編集

--- db/migrate/20210302074003_create_purchasers.rb
+++ db/migrate/20210302074003_create_purchasers.rb
@@ -3,6 +3,14 @@ class CreatePurchasers < ActiveRecord::Migration[6.0]
     create_table :purchasers do |t|
 
       t.timestamps
+      t.string     :post_code,           null: false
+      t.integer    :shipping_area_id,    null: false
+      t.string     :shipping_city,       null: false
+      t.string     :shipping_address,    null: false
+      t.string     :building_name
+      t.string     :phone_number,        null: false
+      t.references :order,               null: false, foreign_key: true
+
     end
   end
 end

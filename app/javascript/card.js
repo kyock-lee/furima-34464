ea17ba0a0eeea94ce9c66eb7942502661dec1089
kyock-lee <yakioni1031@gmailcom>
2021-03-03 16:22:25 +0900

    環境変数の設定
    決済機能

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_cbd7009fca50ce65be27203a"); 
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); 
     const form = document.getElementById("charge-form");
     form.addEventListener("submit", (e) => {
     e.preventDefault();

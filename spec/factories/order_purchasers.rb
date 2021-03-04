e4ce2bfcea65702fa38c467e9327b2cebe961c26
kyock-lee <yakioni1031@gmailcom>
2021-03-04 12:43:51 +0900

    商品購入機能の単体テストコード

--- spec/factories/order_purchasers.rb
+++ spec/factories/order_purchasers.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :order_purchaser do
-    
+    token            {"tok_abcdefghijk00000000000000000"}
+    post_code        { '123-4567' }
+    shipping_area_id { 5 }
+    shipping_city    { '仙台市太白区' }
+    shipping_address { '長町1−1−1' }
+    building_name    { '長町ハイツ' }
+    phone_number     { '01234567811' }
+
+    #formオブジェクトはアソシエーションを組めない
   end
 end

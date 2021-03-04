961d1246f5c22e99b34254a1b6767f458ecd9aed
kyock-lee <yakioni1031@gmailcom>
2021-03-04 14:30:42 +0900

    バリデーションの修正(OrderPurchaser)

--- app/models/order_purchaser.rb
+++ app/models/order_purchaser.rb
@@ -5,7 +5,7 @@ class OrderPurchaser
 
   with_options presence: true do
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :shipping_area_id
+    validates :shipping_area_id, numericality: { other_than: 1 }
     validates :shipping_city
     validates :shipping_address
     validates :phone_number, format: { with: /\A[0-9]{11}+\z/ }
